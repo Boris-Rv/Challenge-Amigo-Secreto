146379cdbd85c21bbf78685ee0eeb1d42f5c2578
Boris Rivera <riveraboris945@gmail.com>
2025-08-20 10:26:25 -0500

    agregar funciones nuevos

--- app.js
+++ app.js
@@ -4,9 +4,22 @@ function agregarAmigo(){
      let ingresarNombres= document.getElementById('amigo').value;
     if(ingresarNombres=== '') {
         alert('Por favor, inserte un nombre');
-        return;
+        
     }
     listasDeNombres.push(ingresarNombres);
+    limpiarCaja();
+    actualizarlista();
+    //console.log(listasDeNombres);
 
+}
+
+function limpiarCaja (){
+    document.querySelector('#amigo').value= '';
+}
+
+
+function mostrarlista() {
+    let lista = document.querySelector('#listaAmigos');
+    lista.innerHTML='';
     
 }
