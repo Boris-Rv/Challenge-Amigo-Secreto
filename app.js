26292318c6b613890e6d321e1e5f2de808ab2963
Boris Rivera <riveraboris945@gmail.com>
2025-08-22 00:52:01 -0500

    funcion agrego para sorteo de amigos

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ function agregarAmigo(){
      let ingresarNombres= document.getElementById('amigo').value;
     if(ingresarNombres=== '') {
         alert('Por favor, inserte un nombre');
+        return;
         
     }
     listasDeNombres.push(ingresarNombres);
     limpiarCaja();
-    actualizarlista();
+    mostrarlista();
+    
+
     //console.log(listasDeNombres);
 
 }
@@ -21,5 +24,23 @@ function limpiarCaja (){
 function mostrarlista() {
     let lista = document.querySelector('#listaAmigos');
     lista.innerHTML='';
+    for(let generarNombre=0; generarNombre<listasDeNombres.length; ++generarNombre){
+    let elemento=document.createElement('li');
+    elemento.textContent=listasDeNombres[generarNombre];
+    lista.appendChild(elemento);
+
+    }
     
 }
+
+function sortearAmigo() {
+    if(listasDeNombres.length>0){
+    let generaramigos=Math.floor(Math.random()*listasDeNombres.length);
+    let obtenerNombre=listasDeNombres[generaramigos];
+    let mostrarResultado=document.getElementById("resultado");
+    mostrarResultado.innerHTML= `<li>El amigo secreto sorteado es: ${obtenerNombre}</li>`;
+
+    }else{
+        alert('agrege un nombre de un amigo');
+        }
+}    
